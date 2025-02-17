e7b05df5baecf14474216ce8965234d713e1116f
fo <alfonsomartingarcia@gmail.com>
2025-02-17 22:56:17 +0000

    specific md stuff

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,12 @@
+-- here will come some options for future markdown settings
+vim.opt.wrap = true
+
+vim.opt.linebreak = true -- Wrap lines at word boundaries
+vim.opt.cursorline = true -- Highlight the current line
+
+vim.opt_local.spell = true
+
+vim.keymap.set("n", "<Tab>", "]s")
+vim.keymap.set("n", "<S-Tab>", "[s")
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
